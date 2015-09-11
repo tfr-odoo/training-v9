9ed11236885b89ba4398d480b876bd68ebab7776
Thibault Francois <tfr@openerp.com>
2015-09-11 17:25:41 +0200

    [EX02] Manifest

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -3,20 +3,19 @@
     'name': "openacademy",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        My first module during training V9""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Odoo - Thibault Francois",
+    'website': "http://www.odoo.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Academy',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
