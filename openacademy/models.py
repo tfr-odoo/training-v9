3e1fb8598c3967be10cacb8e117504c585b2863c
Thibault Francois <tfr@openerp.com>
2015-09-11 18:39:52 +0200

    [EX5] basic view + new field and required

--- openacademy/models.py
+++ openacademy/models.py
@@ -5,4 +5,5 @@ from openerp import models, fields, api
 class openacademy(models.Model):
     _name = 'openacademy.course'
 
-    name = fields.Char(name='Title')
+    name = fields.Char(name='Title', required=True)
+    description = fields.Text()
