d6d81d83eb8ba94f975e630ff14c0951c3afdb0a
Thibault Francois <tfr@openerp.com>
2015-09-11 17:59:01 +0200

    [EX3] model definition

--- openacademy/models.py
+++ openacademy/models.py
@@ -2,14 +2,7 @@
 
 from openerp import models, fields, api
 
-# class openacademy(models.Model):
-#     _name = 'openacademy.openacademy'
+class openacademy(models.Model):
+    _name = 'openacademy.course'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(name='Title')
