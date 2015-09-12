226076558ccf7dbe592f4d6552347244b988b456
Thibault Francois <tfr@openerp.com>
2015-09-12 12:52:54 +0200

    [EX15Bis] more complex onchange

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -16,14 +16,23 @@ class Session(models.Model):
 
     name = fields.Char(required=True)
     start_date = fields.Date(default=lambda self : fields.Date.today())
+    end_date = fields.Date(default=lambda self : fields.Date.today())
     active = fields.Boolean(default=True)
-    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor") #No ondelete = set null
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees", domain=[('company_type', '=', 'person')])
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     level = fields.Selection(related='course_id.level')
+
+    def _warning(self, title, message):
+            return {
+              'warning': {
+                'title': title,
+                'message': message,
+              },
+            }
 
     @api.one
     @api.depends('seats', 'attendee_ids')
@@ -36,15 +45,16 @@ class Session(models.Model):
 
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
-        def _warning(title, message):
-            return {
-              'warning': {
-                'title': title,
-                'message': message,
-              },
-            }
-
         if self.seats < 0:
-            return _warning("Incorrect 'seats' value",  "The number of available seats may not be negative")
+            return self._warning("Incorrect 'seats' value",  "The number of available seats may not be negative")
         if self.seats < len(self.attendee_ids):
-            return _warning("Too many attendees", "Increase seats or remove excess attendees")
+            return self._warning("Too many attendees", "Increase seats or remove excess attendees")
+
+    @api.onchange('start_date', 'end_date')
+    def _compute_duration(self):
+        if not (self.start_date and self.end_date):
+            return
+        if self.end_date < self.start_date:
+            return self._warning("Incorrect date value", "End date is earlier then start date")
+        delta = fields.Date.from_string(self.end_date) - fields.Date.from_string(self.start_date)
+        self.duration = delta.days + 1
