cfb927f601e6b20e69ce643099bd41d2923a1096
Thibault Francois <tfr@openerp.com>
2015-09-12 11:24:43 +0200

    [EX14] default value

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -15,7 +15,8 @@ class Session(models.Model):
     _name = 'openacademy.session'
 
     name = fields.Char(required=True)
-    start_date = fields.Date()
+    start_date = fields.Date(default=lambda self : fields.Date.today())
+    active = fields.Boolean(default=True)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor") #No ondelete = set null
