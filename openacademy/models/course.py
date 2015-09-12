c21158c332fef78fbdaca4a40f768b84d0b25586
Thibault Francois <tfr@openerp.com>
2015-09-12 10:50:39 +0200

    [EX11] simple domain

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -17,6 +17,6 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string="Instructor") #No ondelete = set null
+    instructor_id = fields.Many2one('res.partner', string="Instructor", domain=[('instructor', '=', True)]) #No ondelete = set null
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    attendee_ids = fields.Many2many('res.partner', string="Attendees", domain=[('company_type', '=', 'person')])
