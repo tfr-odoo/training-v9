19bf1286feec680fd3242a048518f21f3040372f
Thibault Francois <tfr@openerp.com>
2015-09-12 14:08:25 +0200

    [EX19] Calendar view + computed fields

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from datetime import timedelta
 from openerp import models, fields, api, exceptions
 
 class openacademy(models.Model):
@@ -40,7 +40,8 @@ class Session(models.Model):
 
     name = fields.Char(required=True)
     start_date = fields.Date(default=lambda self : fields.Date.today())
-    end_date = fields.Date(default=lambda self : fields.Date.today())
+    #end_date = fields.Date(default=lambda self : fields.Date.today())
+    end_date = fields.Date(string='End date', store=True, compute='_get_end_date', inverse='_set_end_date')
     active = fields.Boolean(default=True)
     duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
     seats = fields.Integer(string="Number of seats")
@@ -74,14 +75,14 @@ class Session(models.Model):
         if self.seats < len(self.attendee_ids):
             return self._warning("Too many attendees", "Increase seats or remove excess attendees")
 
-    @api.onchange('start_date', 'end_date')
-    def _compute_duration(self):
-        if not (self.start_date and self.end_date):
-            return
-        if self.end_date < self.start_date:
-            return self._warning("Incorrect date value", "End date is earlier then start date")
-        delta = fields.Date.from_string(self.end_date) - fields.Date.from_string(self.start_date)
-        self.duration = delta.days + 1
+#     @api.onchange('start_date', 'end_date')
+#     def _compute_duration(self):
+#         if not (self.start_date and self.end_date):
+#             return
+#         if self.end_date < self.start_date:
+#             return self._warning("Incorrect date value", "End date is earlier then start date")
+#         delta = fields.Date.from_string(self.end_date) - fields.Date.from_string(self.start_date)
+#         self.duration = delta.days + 1
 
 
     @api.one
@@ -89,3 +90,25 @@ class Session(models.Model):
     def _check_instructor_not_in_attendees(self):
         if self.instructor_id and self.instructor_id in self.attendee_ids:
             raise exceptions.ValidationError("A session's instructor can't be an attendee")
+
+    @api.one
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        if not (self.start_date and self.duration):
+            self.end_date = self.start_date
+            return
+        # Add duration to start_date, but: Monday + 5 days = Saturday, so
+        # subtract one second to get on Friday instead
+        start = fields.Datetime.from_string(self.start_date)
+        duration = timedelta(days=self.duration, seconds=-1)
+        self.end_date = start + duration
+
+    @api.one
+    def _set_end_date(self):
+        if not (self.start_date and self.end_date):
+            return
+        # Compute the difference between dates, but: Friday - Monday = 4 days,
+        # so add one day to get 5 days instead
+        start_date = fields.Datetime.from_string(self.start_date)
+        end_date = fields.Datetime.from_string(self.end_date)
+        self.duration = (end_date - start_date).days + 1
