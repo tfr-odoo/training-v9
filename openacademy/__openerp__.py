41a7c8c57785fb54b58fdc14f1161199e64a88d6
Thibault Francois <tfr@openerp.com>
2015-09-12 08:25:51 +0200

    [EX6] new model + view

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -24,7 +24,8 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views.xml',
+        'views/courses.xml',
+        'views/sessions.xml',
         'templates.xml',
     ],
     # only loaded in demonstration mode
--- openacademy/models.py
+++ openacademy/models.py
@@ -7,3 +7,11 @@ class openacademy(models.Model):
 
     name = fields.Char(name='Title', required=True)
     description = fields.Text()
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
