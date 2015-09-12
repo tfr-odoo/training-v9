a6608116713c50040e5dcf2a6c721c7dd9a536b0
Thibault Francois <tfr@openerp.com>
2015-09-12 11:23:59 +0200

    [EX13] computed field

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -20,3 +20,12 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor") #No ondelete = set null
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees", domain=[('company_type', '=', 'person')])
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.one
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        if not self.seats:
+            self.taken_seats = 0.0
+        else:
+            self.taken_seats = 100.0 * len(self.attendee_ids) / self.seats
