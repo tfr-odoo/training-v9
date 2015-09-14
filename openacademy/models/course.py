192ad86e992a45b6d62267c0e320b704485f387a
Thibault Francois <tfr@openerp.com>
2015-09-13 21:24:43 -0400

    [EX17.2] redefined copy

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -19,6 +19,20 @@ class openacademy(models.Model):
         "The course title must be unique"),
     ]
 
+    @api.one
+    def copy(self, default=None):
+        default = dict(default or {})
+
+        copied_count = self.search_count(
+            [('name', '=like', u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copy of {}".format(self.name)
+        else:
+            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+
+        default['name'] = new_name
+        return super(openacademy, self).copy(default)
+
 class Session(models.Model):
     _name = 'openacademy.session'
 
