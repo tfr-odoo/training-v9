3266a1772131931406787cc6c8b23dc7a0a93dba
Thibault Francois <tfr@openerp.com>
2015-09-13 21:12:19 -0400

    [EX32] validation button on wizard

--- openacademy/wizard/add_attendee.py
+++ openacademy/wizard/add_attendee.py
@@ -10,3 +10,8 @@ class Wizard(models.TransientModel):
 
     session_id = fields.Many2one('openacademy.session', string="Session", required=True, default=_default_session)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    @api.multi
+    def subscribe(self):
+        self.session_id.attendee_ids |= self.attendee_ids
+        return {}
