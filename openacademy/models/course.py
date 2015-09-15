94bcd3d2d50ed88b4d6c18d3e2f7e40f02538c44
Thibault Francois <tfr@openerp.com>
2015-09-15 12:11:03 -0400

    [EX13Bis] more complex computation + related field

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -9,6 +9,7 @@ class openacademy(models.Model):
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+    level = fields.Selection([(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], string="Difficulty Level")
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -21,6 +22,7 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees", domain=[('company_type', '=', 'person')])
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    level = fields.Selection(related='course_id.level')
 
     @api.one
     @api.depends('seats', 'attendee_ids')
--- openacademy/models/partner.py
+++ openacademy/models/partner.py
@@ -7,4 +7,15 @@ class Partner(models.Model):
     # instructors
     instructor = fields.Boolean("Instructor", default=False)
     session_ids = fields.Many2many('openacademy.session', string="Attended Sessions", readonly=True)
+    level = fields.Integer(compute="_get_level", string="Teacher", store=True)
+
+    @api.one
+    @api.depends('category_id', 'category_id.name')
+    def _get_level(self):
+        level = []
+        for categ in self.category_id:
+            if "Teacher / Level" in categ.name:
+                level.append(int(categ.name.split(' ')[-1]))
+
+        self.level = max(level) if level else 0
 
