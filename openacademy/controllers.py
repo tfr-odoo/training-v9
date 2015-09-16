19238c0984d2ba87be95831a95cc52a1612654e2
Thibault Francois <tfr@openerp.com>
2015-09-16 13:07:27 -0400

    [EX42] routing

--- openacademy/controllers.py
+++ openacademy/controllers.py
@@ -10,10 +10,20 @@ class Academy(http.Controller):
             'courses': courses,
         })
 
-    @http.route('/academy/session/', auth='public', website=True)
-    def session(self, **kw):
+    @http.route('/academy/session_list/', auth='public', website=True)
+    def session_list(self, **kw):
         sessions = http.request.env['openacademy.session'].search([])
+        return http.request.render('openacademy.session_list', {
+            'session': sessions
+        })
+
+    @http.route('/academy/<int:id>/', auth='public', website=True)
+    def teacher(self, id):
+        return '<h1>{} ({})</h1>'.format(id, type(id).__name__)
+
+    @http.route('/academy/<model("openacademy.session"):session>/', auth='public', website=True)
+    def session(self, session):
         return http.request.render('openacademy.session', {
-            'sessions': sessions,
+            'session': session
         })
 
