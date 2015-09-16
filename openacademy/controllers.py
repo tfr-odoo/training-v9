2351ba147306482ab6c74592705d3f722615385f
Thibault Francois <tfr@openerp.com>
2015-09-15 23:13:30 -0400

    [EX39] Render template

--- openacademy/controllers.py
+++ openacademy/controllers.py
@@ -4,5 +4,7 @@ from openerp import http
 class Academy(http.Controller):
     @http.route('/academy/academy/', auth='public')
     def index(self, **kw):
-        return "Hello, world"
+        return http.request.render('openacademy.index', {
+            'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"],
+        })
 
